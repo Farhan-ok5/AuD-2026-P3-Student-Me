3d91e3180ff6841bf8380189c3eafb8b63d594f4
Farhan-ok5 <farhanah402@gmail.com>
2026-07-07 00:05:07 +0200

    inserted name and id

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ submission {
     // ACHTUNG!
     // Setzen Sie im folgenden Bereich Ihre TU-ID (NICHT Ihre Matrikelnummer!), Ihren Nachnamen und Ihren Vornamen
     // in Anführungszeichen (z.B. "ab12cdef" für Ihre TU-ID) ein!
-    studentId = "!!!!!ATTENTION-TU_ID NOT MATRIKEL Number"
-    firstName = "!!!!!ATTENTION FIRST NAME"
-    lastName = "!!!!!ATTENTION LAST NAME"
+    studentId = "fa74biza"
+    firstName = "Farhan"
+    lastName = "Ahmed"
     // Optionally require own tests for mainBuildSubmission task. Default is false
     requireTests = false
     // Optionally require public grader for mainBuildSubmission task. Default is false
